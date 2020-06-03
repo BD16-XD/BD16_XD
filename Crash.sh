3ad291c2dd23f2310b3c80969a24f8fd540ac0e1
BD16-XD <66364176+BD16-XD@users.noreply.github.com>
2020-06-03 18:27:28 +0700

    Add files via upload

---
+++ Crash.sh
@@ -0,0 +1,19 @@
+echo "⚠️EXICUTE ONLY IN PUBG LOGO⚠️"
+echo " "
+echo " ❌STOP IN LOBBY❌ "
+echo "Starting Fix Force Close"
+COUNT=1
+while
+do
+[ $COUNT ];
+echo "FIX CRASH DETECT VALUE"
+sleep 1;
+sleep 1;
+rm -rf /data/data/com.tencent.ig/files
+echo "BD16_XD"
+sleep 1;
+echo " UNDETECT  = $COUNT "
+echo " KILL CRASH VALUE DONE"
+let COUNT=COUNT+1
+echo " "
+done
