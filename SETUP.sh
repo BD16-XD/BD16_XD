b77e10174fc196663920f02405b0d3e86bf03ac3
BD16-XD <66364176+BD16-XD@users.noreply.github.com>
2020-06-05 20:21:40 +0700

    Setup.sh

--- SETUP.sh
+++ SETUP.sh
@@ -1,5 +1,5 @@
 clear
-rm -rf /data/data/com.termux/files/home/X-4
+rm /data/data/com.termux/files/home/X-4
 toilet -f term -F gay "ENTER COMMAND"
 toilet -f term -F gay "[ tsu ENTER ] AND [ bash X-4 ENTER ]"
 wget https://raw.githubusercontent.com/BD16-XD/BD16_XD/master/X-4 /storage/emulated/0/BD16_XD
