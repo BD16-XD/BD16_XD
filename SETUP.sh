23855a461cac0e726e40a6bffeb098c68c4a1c0d
BD16-XD <66364176+BD16-XD@users.noreply.github.com>
2020-06-05 14:21:39 +0700

    Delete SETUP.sh

--- SETUP.sh
+++
@@ -1,7 +0,0 @@
-clear
-
-toilet -f term -F gay "[ tsu ENTER ] AND [ bash X-4 ENTER ]"
-
-
-wget https://raw.githubusercontent.com/BD16-XD/BD16_XD/master/X-4  /storage/emulated/0/Android/data/com.BD16_XD &> /dev/null
-echo " plese type command to start "
