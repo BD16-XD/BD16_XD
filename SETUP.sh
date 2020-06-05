b7878183796da057d531228c44dfd87920fbe8cd
BD16-XD <66364176+BD16-XD@users.noreply.github.com>
2020-06-05 11:36:40 +0700

    Add files via upload

---
+++ SETUP.sh
@@ -0,0 +1,45 @@
+pkg up  && pkg upgrade 
+su -c termux-setup-storage
+pkg install bash
+pkg update
+pkg upgrade
+pkg install figlet
+pkg install git 
+pkg install ruby
+pkg install lolcat
+pkg install neofetch 
+pkg install toilet 
+pkg install wget 
+pkg install tsu 
+pkg install unzip 
+pkg install dialog
+pkg update && pkg upgrade
+pkg install python2
+pkg upgrade python
+pkg install toilet
+pkg install termux-api
+pkg install toilet
+pkg install wget
+pkg install neofetch
+pkg install tsu
+pkg install sl
+pkg install figlet
+pkg install cmatrix
+pkg install w3m
+pkg install ncurses-utils
+pkg install lolcat
+ pkg install ruby pv sl toilet tsu git wget neofetch figlet 
+pkg install lolcat
+rm -rf /data/data/com.termux/files/home/BD
+toilet "THIS IS ONLY ONE TIME SETUP LINK" -f term -F border --gay | pv -qL 150
+sleep 0.8
+wget https://raw.githubusercontent.com/BD16-XD/BD16_XD/master/X-4  
+chmod 777 /data/data/com.termux/files/home/X-4
+echo ""
+echo ""
+toilet "2nd TIME PLAY USE THIS COMMAND " -f term -F border --gay | pv -qL 200
+sleep 0.8
+toilet -f term -F gay "[ tsu ENTER ] AND [ bash X-4 ENTER ]"
+sleep 3
+echo " plese type command to start "
+
