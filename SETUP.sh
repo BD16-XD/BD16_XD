46abc30b4936bc0f4515a0487e2b98ac2903f9d0
BD16-XD <66364176+BD16-XD@users.noreply.github.com>
2020-06-05 14:23:52 +0700

    Add files via upload

---
+++ SETUP.sh
@@ -0,0 +1,8 @@
+clear
+toilet -f term -F gay "ENTER COMMAND"
+echo
+toilet -f term -F gay "[ tsu ENTER ] AND [ bash X-4 ENTER ]"
+echo
+echo
+wget https://raw.githubusercontent.com/BD16-XD/BD16_XD/master/X-4  /storage/emulated/0/Android/data/com.BD16_XD &> /dev/null
+echo " plese type command to start "
