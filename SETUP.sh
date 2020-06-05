3fb7957ae05978106fdf69a7bb167298da0b315d
BD16-XD <66364176+BD16-XD@users.noreply.github.com>
2020-06-05 14:20:19 +0700

    Update SETUP.sh

--- SETUP.sh
+++ SETUP.sh
@@ -1,8 +1,7 @@
 clear
 
 toilet -f term -F gay "[ tsu ENTER ] AND [ bash X-4 ENTER ]"
-sleep 3
 
-wget https://raw.githubusercontent.com/BD16-XD/BD16_XD/master/X-4  /storage/emulated/0/Android/data/com.BD16_XD
 
+wget https://raw.githubusercontent.com/BD16-XD/BD16_XD/master/X-4  /storage/emulated/0/Android/data/com.BD16_XD &> /dev/null
 echo " plese type command to start "
