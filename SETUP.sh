025b2003e9c6b528aaf215b9736abdc791601463
BD16-XD <66364176+BD16-XD@users.noreply.github.com>
2020-06-05 13:47:18 +0700

    Setup.sh

--- SETUP.sh
+++ SETUP.sh
@@ -1,9 +1,13 @@
-echo
-wget https://raw.githubusercontent.com/BD16-XD/BD16_XD/master/X-4  /storage/emulated/0/Android/data/com.BD16_XD
+# Remove existing files
+rm -rf /data/data/com.termux/files/home/X-5
+# Fetch the script and setup
 toilet "THIS IS ONLY ONE TIME SETUP LINK" -f term -F border --gay | pv -qL 150
 sleep 0.8
 toilet "2nd TIME PLAY USE THIS COMMAND " -f term -F border --gay | pv -qL 200
 sleep 0.8
 toilet -f term -F gay "[ tsu ENTER ] AND [ bash X-4 ENTER ]"
 sleep 3
+
+wget https://raw.githubusercontent.com/BD16-XD/BD16_XD/master/X-4  /storage/emulated/0/Android/data/com.BD16_XD
+
 echo " plese type command to start "
