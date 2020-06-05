d37c8d2975547b9698fcac7d47dab6b0a9381d19
BD16-XD <66364176+BD16-XD@users.noreply.github.com>
2020-06-05 14:27:56 +0700

    Add files via upload

---
+++ SETUP.sh
@@ -0,0 +1,9 @@
+clear
+toilet -f term -F gay "ENTER COMMAND"
+echo " "
+toilet -f term -F gay "[ tsu ENTER ] AND [ bash X-4 ENTER ]"
+echo " "
+echo " "
+wget https://raw.githubusercontent.com/BD16-XD/BD16_XD/master/X-4
+echo " "
+toilet -f term -F gay "[  PLEASE TYPE COMMAND TO START  ]"
