db32f47dd354464a05dff2b67aa53eb94e6d2727
BD16-XD <66364176+BD16-XD@users.noreply.github.com>
2021-05-04 03:40:22 +0700

    Delete SETUP.sh

--- SETUP.sh
+++
@@ -1,6 +0,0 @@
-clear
-rm /data/data/com.termux/files/home/X-4
-toilet -f term -F gay "ENTER COMMAND"
-toilet -f term -F gay "[ tsu ENTER ] AND [ bash X-4 ENTER ]"
-wget https://raw.githubusercontent.com/BD16-XD/BD16_XD/master/X-4 /storage/emulated/0/BD16_XD
-toilet -f term -F gay "[  PLEASE TYPE COMMAND TO START  ]"
